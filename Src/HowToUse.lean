9009a81bd4cbdd3a426be46ca9cb975b3eac1755
Seasawher <tellagoodstory@icloud.com>
2024-07-04 20:34:03 +0900

    enable link checker
    resolve #14

--- Src/HowToUse.lean
+++ Src/HowToUse.lean
@@ -6,11 +6,13 @@
 
 2. Change the package name `«Lean Book»` as you like. And run `lake update`.
 
-3. If you want to use mathlib, make changes to `ci.yml` to run `lake exe cache get`.
+3. Change the `site_url` in `book.toml` to your GitHub page's URL.
 
-4. After updating the Lean file, run `lake run build`. This will generate markdown with mdgen, HTML with mdbook and import statements with import-check.
+4. Edit `.lycheeignore` file. This controls which URL is ignored by the link checker.
 
-5. Run `mdbook serve --open` to see the preview.
+5. After updating the Lean file, run `lake run build`. This will generate markdown with mdgen, HTML with mdbook and import statements with import-check.
 
-6. Push to GitHub. The workflow for deploy has already been set up, so you can publish it as a web page by enabling deploy to GitHub Pages via GitHub Action from the repository settings.
+6. Run `mdbook serve --open` to see the preview.
+
+7. Push to GitHub. The workflow for deploy has already been set up, so you can publish it as a web page by enabling deploy to GitHub Pages via GitHub Action from the repository settings.
 -/
