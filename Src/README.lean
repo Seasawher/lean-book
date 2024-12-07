49bb61ee2f9daba79ab80277232de17529de7f19
Seasawher <tellagoodstory@icloud.com>
2024-12-07 16:15:45 +0900

    add table of contents to each page

--- Src/README.lean
+++ Src/README.lean
@@ -1,11 +1,10 @@
 /- # Lean Book
 Lean Book is the mdbook template configured for the Lean Project. Intended for use in writing textbooks and documentation in Lean.
 
-## Features
 Lean Book has a variety of features.
 -/
 
-/-! ### Syntax Highlighting for Lean Code
+/- ## Syntax Highlighting for Lean Code
 Lean syntax highlighting works in this book. Here is an example of Lean code block.-/
 
 variable {P Q : Prop}
@@ -15,19 +14,23 @@ example : P → (P ∨ Q) := by
   apply Or.inl
   exact hP
 
-/-! ### Run Lean4 Code Block
+/- ## Run Lean4 Code Block
 All Lean code blocks in this book have a button that jumps to the Lean Playground.
 
-### Jump to Lean Playground from any page
+## Jump to Lean Playground from any page
 Jump to the Lean4 Playground by clicking on the play button in the top right corner of a page.
 
-### Easy Code Management
+## Table of Contents
+
+Automatically add a table of contents to each page.
+
+## Easy Code Management
 The markdown files in this book are generated from Lean code using [mdgen](https://github.com/Seasawher/mdgen). There is no need to maintain separately the code examples and the text.
 
-### Improved Search
+## Improved Search
 The mdbook does not support searches in languages other than English, but this book does support searches in other languages (e.g. Japanese)!
 
-### MathJax
+## MathJax
 You can use MathJax in this book. For example, $a^2 + b^2 = c^2$.
 
 $$
